9a59367c7c0c3dd50186332e2caba1e80fbee22c
Mahek Shakil Shikalgar <111283581+MahekShikalgar2@users.noreply.github.com>
2025-06-06 20:27:25 +0530

    Add files via upload

---
+++ Day55_Solution.java
@@ -0,0 +1,25 @@
+
+class Solution {
+    // Function to count the number of possible triangles.
+    static int countTriangles(int arr[]) {
+        Arrays.sort(arr);
+        int count = 0;
+        
+        for(int i=arr.length-1; i>=2; i--){
+            int left = 0;
+            int right = i-1;
+            
+            while(left<right){
+                if(arr[left]+arr[right]>arr[i]){
+                    count += right-left;
+                    right--;
+                }else{
+                    left++;
+                }
+            }
+        }
+        return count;
+            
+        
+    }
+}
