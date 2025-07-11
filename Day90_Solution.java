abc75016e7095d0daa78cc3f0a98e0af4fd18cfb
Mahek Shakil Shikalgar <111283581+MahekShikalgar2@users.noreply.github.com>
2025-07-11 15:54:22 +0530

    Add files via upload

---
+++ Day90_Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    int count = 0;
+    int result = -1;
+    public int kthSmallest(Node root, int k) {
+        inorder(root, k);
+        return result;
+    }
+    void inorder(Node node, int k){
+        if(node == null || count >= k){
+            return;
+        }
+        inorder(node.left, k);
+        count++;
+        if(count == k){
+            result = node.data;
+            return;
+        }
+        inorder(node.right, k);
+    }
+}
