cbd406824722b44d02e035a511adf5a85e378d6f
Mahek Shakil Shikalgar <111283581+MahekShikalgar2@users.noreply.github.com>
2025-08-04 21:42:53 +0530

    Add files via upload

---
+++ Day114.Solution.java
@@ -0,0 +1,37 @@
+class Solution {
+    static String longestPalindrome(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int maxLength = 1;
+        int start = 0;
+        
+        for(int i=0; i<n; i++){
+            dp[i][i] = true;
+        }
+            for(int i = 0; i<n-1; i++){
+                if(s.charAt(i) == s.charAt(i + 1)){
+                    dp[i][i + 1] = true;
+                    if(2 > maxLength){
+                    maxLength = 2;
+                    start = i;
+                    
+                    }
+                }
+                
+            }
+        
+            for(int len = 3; len<=n; len++){
+                for(int i=0; i<=n-len; i++){
+                    int j=i+len-1;
+                    if(s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]){
+                        dp[i][j] = true;
+                        if(len > maxLength){
+                            maxLength = len;
+                            start = i;
+                        }
+                    }
+                }
+            }
+            return s.substring(start, start + maxLength);
+        }
+    }
