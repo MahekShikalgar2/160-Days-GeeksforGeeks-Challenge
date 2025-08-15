c08c159bb56f2682a5be9e94835c51533fe9fc0e
Mahek Shakil Shikalgar <111283581+MahekShikalgar2@users.noreply.github.com>
2025-08-15 21:15:49 +0530

    Add files via upload

---
+++ Day125_Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    static int maxProfit(int prices[], int k) {
+        int n = prices.length;
+        k = Math.min(k, n/2);
+        int[][] dp = new int[k + 1][n];
+        
+        for(int i=1; i<=k; i++){
+            int maxDiff = -prices[0];
+            for(int j=1; j<n; j++){
+                dp[i][j] = Math.max(dp[i][j - 1], prices[j] + maxDiff);
+                maxDiff = Math.max(maxDiff, dp[i - 1][j] - prices[j]);
+            }
+        }
+        return dp[k][n - 1];
+    }
+}
