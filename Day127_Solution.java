30815ca27c7859040612a623bd405c5c5e140655
Mahek Shakil Shikalgar <111283581+MahekShikalgar2@users.noreply.github.com>
2025-08-17 21:33:01 +0530

    Add files via upload

---
+++ Day127_Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMaxSum(int arr[]) {
+       int n = arr.length;
+       if(n == 0)return 0;
+       if(n == 1)return arr[0];
+       
+       int last = arr[0];
+       int secondLast = 0;
+       int res = arr[0];
+       
+       for(int i=1; i<n; i++){
+           res = Math.max(arr[i] + secondLast, last);
+           secondLast = last;
+           last = res;
+       }
+       return res;
+     }
+}
