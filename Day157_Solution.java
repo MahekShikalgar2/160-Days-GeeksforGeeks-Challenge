9d81aa30afa7e023ecbff36eb953f0c65ac141f2
Mahek Shakil Shikalgar <111283581+MahekShikalgar2@users.noreply.github.com>
2025-09-15 22:37:48 +0530

    Add files via upload

---
+++ Day157_Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findDuplicate(int[] arr) {
+       for(int i=0; i<arr.length; i++){
+           int index = Math.abs(arr[i]);
+           if(arr[index - 1]<0){
+               return index;
+           }
+           arr[index - 1] *= -1;
+       }
+       return -1;
+        
+    }
+}
